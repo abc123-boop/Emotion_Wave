442bf1f370e7acb66a14c06eaf2d2f16fcbe3127
abc123-boop <grdivya183@gmail.com>
2025-03-22 03:15:29 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Update package list and install dependencies
 apt-get update && apt-get install -y portaudio19-dev
+# Install dependencies
 pip install -r requirements.txt
+# Install PyAudio separately
 pip install pyaudio
